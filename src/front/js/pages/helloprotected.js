541ff3bf0a2503da1a8fb0f90e4f8c90a115b07d
yanzacd92 <121258474+yanzacd92@users.noreply.github.com>
2023-05-23 23:15:45 +0000

    added hello protected

--- src/front/js/pages/helloprotected.js
+++ src/front/js/pages/helloprotected.js
@@ -1,27 +1,17 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
 export const HelloProtected = () => {
 	const { store, actions } = useContext(Context);
-	const navigate = useNavigate();
-	async function submitForm(e) {
-		e.preventDefault()
-		let data = new FormData(e.target)
-		let resp = await actions.userLogin(data.get("email"), data.get("password"))
-		if(resp >= 400) {
-			return
-		}
-		console.log("Login exitoso!!")
-	}
+    let resp = actions.userHelloProtected()
 
 	return (
-		<div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Hello Protected title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/" class="btn btn-primary">Home - Login</a>
+		<div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Hello Protected</h5>
+                <p className="card-text">{resp.msg}</p>
+                <a href="/" className="btn btn-primary">Home - Login</a>
             </div>
         </div>
 	);
